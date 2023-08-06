8d74d90bd3ff086524f0cf96ff917cd7a4ae0f86
Vincent Le Du <vincent.ledu@outlook.com>
2023-08-07 00:22:48 +0200

    improved zoom

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-// use image::{Rgba, imageops, DynamicImage};
-// use imageproc::geometric_transformations::{Projection, Interpolation, warp};
-use argparse::{ArgumentParser, Store, StoreOption, StoreTrue, List };
+use argparse::{ArgumentParser, Store, StoreTrue, List };
 use opencv::{
     imgcodecs, 
     highgui, 
@@ -11,14 +9,37 @@ use opencv::{
     types::VectorOfPoint2f,
 };
 use std::error;
+use num_derive::FromPrimitive;
 
-fn parse_markers(points: &Vector<VectorOfPoint2f>, marker_ids : &Vector<i32>) -> Result<VectorOfPoint2f, opencv::Error> {
+#[derive(FromPrimitive)]
+enum FOVMode {
+    Large = 0,
+    Medium = 1,
+    Zoom = 2,
+}
+
+fn parse_markers(points: &Vector<VectorOfPoint2f>, marker_ids : &Vector<i32>, fov : &FOVMode) -> Result<VectorOfPoint2f, opencv::Error> {
     // Crée un nouveau vecteur réorganisé en suivant les indices du vecteur marker_ids
 
     let mut reordered_points: VectorOfPoint2f = VectorOfPoint2f::from_elem(Point2f::new(0., 0.), 4);
     for (i, new_idx) in marker_ids.iter().enumerate() { 
-        let point_vec = points.get(i)?;
-        let point = point_vec.get(new_idx as usize)?;
+        let point = match fov {
+            FOVMode::Large => points.get(i)?.get(new_idx as usize)?,
+            FOVMode::Medium => {
+                let marker_corners = points.get(i)?;
+                match new_idx {
+                    0 | 3 => (marker_corners.get(0)? + marker_corners.get(3)?) / 2.,
+                    1 | 2 | _ => (marker_corners.get(1)? + marker_corners.get(2)?) / 2.
+                }
+            },
+            FOVMode::Zoom => points.get(i)?.get(
+                match new_idx {
+                    0 | 1 => 3,
+                    2 | 3 | _ => 0, // Cannot be called => only 4 corners
+                } as usize)?
+        };
+        // let point_vec = points.get(i)?;
+        // let point = point_vec.get(new_idx as usize)?;
         reordered_points.set(new_idx as usize, point)?;
     }
     Ok(reordered_points)
@@ -84,10 +105,11 @@ fn show_image(image : &Mat) -> Result<(), opencv::Error> {
 fn main() -> Result<(), Box<dyn error::Error>>{
     // let mut verbose = false;
     let mut input_path = String::new();
-    let mut opt_output_path: Option<String> = None;
-    let mut out_dim = vec![600, 300];
+    // let mut opt_output_path: Option<String> = None;
+    let mut output_paths: Vec<String> = vec![];
+    let mut out_dim = vec![600, 400];
     let mut show = false;
-    
+    let mut fov_vec : Vec<u32> = vec![0];
 
     {
         let mut parser = ArgumentParser::new();
@@ -102,38 +124,44 @@ fn main() -> Result<(), Box<dyn error::Error>>{
             "Input image path to preprocess.")
             .required();
         
-        parser.refer(&mut opt_output_path)
-            .add_option(&["-o", "--img_out"], StoreOption,
+        parser.refer(&mut output_paths)
+            .add_option(&["-o", "--img_out"], List,
             "Output preprocessed image path.");
 
         parser.refer(&mut out_dim)
             .add_option(&["-d", "--out_dim"], List,
-            "Output image dimensions width height (e.g. default is '-d 600 300').");
+            "Output image dimensions width height (e.g. default is '-d 600 400').");
+
+        parser.refer(&mut fov_vec)
+            .add_option(&["-z", "--zoom"], List,
+            "The zoom to apply. Three possible values are 0 (large), 1 (medium) and 2 (zoom). Multiple zoom values can be given. Default is 1 (medium).");
         
-            
         parser.refer(&mut show)
-        .add_option(&["-s", "--show"], StoreTrue,
-        "Show the image in a window instead of saving it. Once the windows is open, press 'q' to quit.");
-        
+            .add_option(&["-s", "--show"], StoreTrue,
+            "Show the image in a window instead of saving it. Once the windows is open, press 'q' to quit.");
+            
         parser.parse_args_or_exit();
     }
 
     println!("Input path: {input_path:?}");
     
-    let mut output_path = String::new(); 
-    if !show {
-        output_path = match opt_output_path {
-            Some(path) => path,
+    // let mut output_paths: Vec<String> = vec![];
+    
+    
+    for (i, &fov_int) in fov_vec.iter().enumerate() {
+        match output_paths.get(i) {
+            Some(_) => (),
             None => {
                 let (base_path, extenstion) = input_path.rsplit_once('.').ok_or(format!("Input file {input_path:?}"))?;
-                let out_path = format!("{base_path}_preproc.{extenstion}");
+                let out_path = format!("{base_path}_preproc_z{fov_int}.{extenstion}");
                 println!("Output path was not specified so image will be written to {out_path}");
-                out_path
-            },
-        };
-        println!("Output path: {output_path:?}");
-    }
-    
+                output_paths.push(out_path);
+                ()
+            }
+        }
+    };
+    println!("Output path: {output_paths:?}");
+
     // let img = get_image(&input_path).to_rgba8();    
     let img = imgcodecs::imread(&input_path, imgcodecs::IMREAD_UNCHANGED)?;
     let out_size = Size::new(out_dim[0], out_dim[1]);
@@ -142,59 +170,50 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     let mut gray_image = Mat::default();
     imgproc::cvt_color(&img, &mut gray_image, imgproc::COLOR_BGR2GRAY, 0)?;
     // show_image(&gray_image);
-
     let aruco_detector = ArucoDetector::new(  
         &get_predefined_dictionary(PredefinedDictionaryType::DICT_4X4_50)?,
         &DetectorParameters::default().unwrap(),
         RefineParameters::new(10., 3., true).unwrap(),
     ).unwrap();
-
     let mut markers_coor : Vector<VectorOfPoint2f> = Vector::new();
     let mut marker_ids: Vector<i32> = Vector::new();
     let mut rejected_img_points: Vector<VectorOfPoint2f> = Vector::new();
     aruco_detector.detect_markers(&gray_image, &mut markers_coor, &mut marker_ids, &mut rejected_img_points)?;
-    
     println!("Markers found: {:?}", marker_ids);
+    
+    for (fov_int, out_path) in fov_vec.iter().zip(output_paths.iter()) {
 
-    if markers_coor.len() == 4 {
-        // println!("{:?}", markers_coor);
-        let ordered_points = parse_markers(&markers_coor, &marker_ids)?;
-
-        // let ordered_points = parse(&marker_points, &marker_ids);
-        println!("Points used from marker 0 to 3: {:?}", ordered_points);
-
+        let fov = num::FromPrimitive::from_u32(fov_int.to_owned()).ok_or(format!("Input fov must be in [0, 1, 2], input was : {fov_int:?}"))?;
         
-        let warped_image = correct_image(&img, &ordered_points, &out_size)?;
-        
-        // let final_image = match Mat::roi(&warped_image, Rect {
-        //     x: 0,
-        //     y: 0,
-        //     width: out_size.width,
-        //     height: out_size.height,
-        // }){
-        //     Ok(img) => img,
-        //     Err(err) => {
-        //         println!("{:?}", err);
-        //         warped_image
-        //     }
-        // };
-        let final_image = Mat::roi(&warped_image, Rect {
-            x: 0,
-            y: 0,
-            width: out_size.width,
-            height: out_size.height,
-        }).unwrap();
+        if markers_coor.len() == 4 {
+            // println!("{:?}", markers_coor);
+            let ordered_points = parse_markers(&markers_coor, &marker_ids, &fov)?;
+    
+            // let ordered_points = parse(&marker_points, &marker_ids);
+            println!("Points used from marker 0 to 3: {:?}", ordered_points);
+    
+            
+            let warped_image = correct_image(&img, &ordered_points, &out_size)?;
 
-        if show {
-            show_image(&final_image)?;
+            let final_image = Mat::roi(&warped_image, Rect {
+                x: 0,
+                y: 0,
+                width: out_size.width,
+                height: out_size.height,
+            }).unwrap();
+    
+            if show {
+                show_image(&final_image)?;
+            } else {
+                println!("Saving image to {:?}", out_path);
+                imgcodecs::imwrite(&out_path, &final_image, &Vector::new())?;
+    
+            }
+
         } else {
-            println!("Saving image to {:?}", output_path);
-            imgcodecs::imwrite(&output_path, &final_image, &Vector::new())?;
-
+            return Err(format!("Error: {:?} markers were found instead of 4.", markers_coor.len()).into())
         }
 
-        Ok(())
-    } else {
-        Err(format!("Error: {:?} markers were found instead of 4.", markers_coor.len()).into())
-    }
+    };
+    return Ok(())
 }
