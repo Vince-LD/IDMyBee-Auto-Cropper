467f8e07b345abe13fb67a293c7e5031f6071973
Vincent Le Du <vincent.ledu@outlook.com>
2023-08-07 23:15:57 +0200

    better error handling and better zoom feature

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ use opencv::{
     objdetect::*,
     types::VectorOfPoint2f,
 };
-use std::error;
+
 use num_derive::FromPrimitive;
+use anyhow::{Result, Error};
 
 #[derive(FromPrimitive)]
 enum ZoomMode {
@@ -18,36 +19,22 @@ enum ZoomMode {
     Zoom = 2,
 }
 
-fn parse_markers(points: &Vector<VectorOfPoint2f>, marker_ids : &Vector<i32>, zoom : &ZoomMode) -> Result<VectorOfPoint2f, opencv::Error> {
+fn parse_markers(points: &Vector<VectorOfPoint2f>, marker_ids : &Vector<i32>) -> Result<VectorOfPoint2f, opencv::Error> {
     // Crée un nouveau vecteur réorganisé en suivant les indices du vecteur marker_ids
 
     let mut reordered_points: VectorOfPoint2f = VectorOfPoint2f::from_elem(Point2f::new(0., 0.), 4);
     for (i, new_idx) in marker_ids.iter().enumerate() { 
-        let point = match zoom {
-            ZoomMode::Large => points.get(i)?.get(new_idx as usize)?,
-            ZoomMode::Medium => {
-                let marker_corners = points.get(i)?;
-                match new_idx {
-                    0 | 3 => (marker_corners.get(0)? + marker_corners.get(3)?) / 2.,
-                    1 | 2 | _ => marker_corners.iter().fold(Point2f::default(), |p_coor, p| p_coor + p) / 4.
-                }
-            },
-            ZoomMode::Zoom => points.get(i)?.get(
-                match new_idx {
-                    0 | 1 => 3,
-                    2 | 3 | _ => 0,
-                } as usize)?
-        };
+        let point = points.get(i)?.get(new_idx as usize)?;        
         reordered_points.set(new_idx as usize, point)?;
     }
     Ok(reordered_points)
 }
 
-fn resize_if_larger_dims(img : &Mat, dims : &Size) -> Result<Mat, opencv::Error> {
+fn resize_if_larger_dims(img : &Mat, out_size : &Size) -> Result<Mat, opencv::Error> {
     // Resize l'image si elle est plus petite que les dimensions d'output
     let img_size = img.size()?;
-    let width_ratio : f64 = (dims.width as f64 / img_size.width as f64).max(1.);
-    let height_ratio : f64 = (dims.height as f64 / img_size.height as f64).max(1.);
+    let width_ratio : f64 = ((out_size.width / img_size.width) as f64).max(1.);
+    let height_ratio : f64 = ((out_size.height / img_size.height) as f64).max(1.);
 
     let mut resized_img = img.clone();
     println!("Original image size {:?}", img.size()?);
@@ -64,13 +51,20 @@ fn resize_if_larger_dims(img : &Mat, dims : &Size) -> Result<Mat, opencv::Error>
     Ok(resized_img)
 }
 
-fn correct_image(img : &Mat, points : &VectorOfPoint2f, out_size: &Size) -> Result<Mat, opencv::Error> {
+fn correct_image(img : &Mat, points : &VectorOfPoint2f, out_size: &Size, zoom: &f32) -> Result<Mat, opencv::Error> {
+
+    let h = out_size.height as f32;
+    let w = out_size.width as f32;
+    let top_y = -0.5 * h * (zoom - 1.);
+    let bot_y = h + 0.5 * h * (zoom - 1.);
+    let right_x = w * zoom;
+
     let target_points: VectorOfPoint2f = Vector::from_slice(
         &[
-            Point2f::new(0.0, 0.0),
-            Point2f::new(out_size.width as f32, 0.0),
-            Point2f::new(out_size.width as f32, out_size.height as f32),
-            Point2f::new(0.0, out_size.height as f32),
+            Point2f::new(0.0, top_y),
+            Point2f::new(right_x, top_y),
+            Point2f::new(right_x, bot_y),
+            Point2f::new(0.0, bot_y),
         ]
     );
 
@@ -100,23 +94,19 @@ fn show_image(image : &Mat) -> Result<(), opencv::Error> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn error::Error>>{
+fn main() -> Result<()>{
     // let mut verbose = false;
     let mut input_path = String::new();
     // let mut opt_output_path: Option<String> = None;
     let mut output_paths: Vec<String> = vec![];
     let mut out_dim = vec![600, 400];
     let mut show = false;
-    let mut zoom_vec : Vec<u32> = vec![0];
+    let mut zoom_vec : Vec<f32> = vec![1.];
 
     {
         let mut parser = ArgumentParser::new();
         parser.set_description("This tools is used to preprcocess photos taken with the ID My Bee protocol. It automatically crop and correct the photo angle.");
-        
-        // parser.refer(&mut verbose)
-        //     .add_option(&["-v", "--verbose"], StoreTrue,
-        //     "Activate verbose");
-        
+
         parser.refer(&mut input_path)
             .add_option(&["-i", "--img"], Store,
             "Input image path to preprocess.")
@@ -145,15 +135,23 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     
     // let mut output_paths: Vec<String> = vec![];
     if !output_paths.is_empty() && output_paths.len() != zoom_vec.len() {
-        return Err(format!("Mismatch between the number of output paths (={:?}) and the number of zoom values (={:?})", output_paths.len(), zoom_vec.len()).into())
+        return Err(
+            anyhow::anyhow!(
+                "Mismatch between the number of output paths (={:?}) and the number of zoom values (={:?})", 
+                output_paths.len(), 
+                zoom_vec.len()
+            )
+        )
     }
     
-    for (i, &zoom_int) in zoom_vec.iter().enumerate() {
+    for (i, &zoom) in zoom_vec.iter().enumerate() {
         match output_paths.get(i) {
             Some(_) => (),
             None => {
-                let (base_path, extenstion) = input_path.rsplit_once('.').ok_or(format!("Input file {input_path:?}"))?;
-                let out_path = format!("{base_path}_preproc_z{zoom_int}.{extenstion}");
+                let (base_path, extenstion) = input_path.rsplit_once('.').ok_or(format!("Input file {input_path:?}")).map_err(Error::msg)?;
+
+                let out_path = format!("{base_path}_preproc_z{:.2}.{extenstion}", zoom).replacen(".", "-", 1);
+                println!("{base_path}_preproc_z{:.3}.{extenstion}", zoom);
                 println!("Output path was not specified so image will be written to {out_path}");
                 output_paths.push(out_path);
                 ()
@@ -181,19 +179,17 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     aruco_detector.detect_markers(&gray_image, &mut markers_coor, &mut marker_ids, &mut rejected_img_points)?;
     println!("Markers found: {:?}", marker_ids);
     
-    for (zoom_int, out_path) in zoom_vec.iter().zip(output_paths.iter()) {
-
-        let zoom = num::FromPrimitive::from_u32(zoom_int.to_owned()).ok_or(format!("Input zoom must be in [0, 1, 2], input was : {zoom_int:?}"))?;
+    for (zoom, out_path) in zoom_vec.iter().zip(output_paths.iter()) {
         
         if markers_coor.len() == 4 {
             // println!("{:?}", markers_coor);
-            let ordered_points = parse_markers(&markers_coor, &marker_ids, &zoom)?;
+            let ordered_points = parse_markers(&markers_coor, &marker_ids)?;
     
             // let ordered_points = parse(&marker_points, &marker_ids);
             println!("Points used from marker 0 to 3: {:?}", ordered_points);
     
             
-            let warped_image = correct_image(&img, &ordered_points, &out_size)?;
+            let warped_image = correct_image(&img, &ordered_points, &out_size, &zoom)?;
 
             let final_image = Mat::roi(&warped_image, Rect {
                 x: 0,
@@ -211,7 +207,7 @@ fn main() -> Result<(), Box<dyn error::Error>>{
             }
 
         } else {
-            return Err(format!("Error: {:?} markers were found instead of 4.", markers_coor.len()).into())
+            return Err(anyhow::anyhow!("Error: {:?} markers were found instead of 4.", markers_coor.len()))
         }
 
     };
